497cc005cc891b04eec81b583af26f57884a202f
JangDongyul <purplebeige12@gmail.com>
2023-04-25 01:56:07 +0900

    교점에 별만들기 2번째 코드에 주석

--- 교점에별만들기.java
+++ 교점에별만들기.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution1 {
+
+    //선분의 교점
     class Point{
         long x, y;
         Point(long x, long y){
@@ -14,12 +16,20 @@ class Solution1 {
     Point intersection(long A, long B, long E, long C, long D, long F){
         double x = (double)(B*F-E*D)/(double)(A*D-B*C);
         double y = (double)(E*C-A*F)/(double)(A*D-B*C);
+        //여기서 double 형변환을 왜 하냐면
+        //정수 타입인 int 또는 long은 나누기 0 하면 ArithmeticException: / by zero 예외 생김
+        //실수 타입인 float 또는 double은 나누기 0 해도 예외 안생김
+        //부동소수점이라 그렇다
+
         if((x%1!=0) || (y%1!=0)){
+            //교차점 중 하나라도 정수가 아니면
             return null;
         }
+        //정수면 %1 =0 이 나와야 한다. 음수도 그렇대
         return new Point((long)x,(long)y);
     }
 
+    //최소의 x,y만 리턴
     Point getMinimumPoint(List<Point> points){
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
@@ -35,6 +45,7 @@ class Solution1 {
         return  new Point(x,y);
     }
 
+    //최대의 x,y를 리턴
     Point getMaximumPoint(List<Point> points){
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
@@ -52,11 +63,12 @@ class Solution1 {
 
     public String[] solution(int[][] line) {
         List<Point> points = new ArrayList<>();
-
-        for(int i=0; i<line.length; i++){
+        //교차점들의 집합
+        for(int i=0; i<line.length; i++){//모든 선분에 대한 교차점 조사
             for(int j=i+1; j<line.length; j++){
                 Point point = intersection(line[i][0],line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
                 if(point == null){
+                    //정수좌표 교차점이 없으면
                     continue;
                 }
                 points.add(point);
